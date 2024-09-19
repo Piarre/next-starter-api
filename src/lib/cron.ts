fa5b8b134764cc7248dec3d47bcacd39954a0c5a
Piarre <pierre@ideestore.fr>
2024-09-19 16:05:47 +0200

    chore: refactor cron job

--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -2,7 +2,7 @@ import { CronJob } from "cron";
 import { rmSync } from "node:fs";
 import { QUEUE, TEMP_DIR } from "..";
 
-const job = new CronJob("*/5 * * * *", async () => {
+export default new CronJob("*/5 * * * *", async () => {
   const dirToDelet = QUEUE.filter((dir) => Date.now() - dir.date > 5 * 60 * 1000);
 
   for (const dir of dirToDelet) {
@@ -11,5 +11,3 @@ const job = new CronJob("*/5 * * * *", async () => {
     QUEUE.splice(QUEUE.indexOf(dir), 1);
   }
 });
-
-export default job;
